b2d9900ba47ef5fa681412371bc30253ace1dc08
JorgeT94 <JLTS.01.94@gmail.com>
2016-04-12 10:59:11 -0600

    Archivo JS, funcion anonima y consola

---
+++ javascript/js/codigo.js
@@ -0,0 +1,9 @@
+var botonClic = function(){
+	alert('Me dio clic');
+}
+// Procedimiento no regresa valor. Función si regresa valor
+var nombre = "Jorge";
+document.write("<strong>JS en acción</strong>"+" "+nombre);
+console.log("A punto de lanzar un mensaje");
+// Pantalla modal
+alert("Mensaje por pantalla");
