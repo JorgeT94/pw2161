5e402abbe71d3a8fcfbdc1ac3d3ab648753fef91
JorgeT94 <JLTS.01.94@gmail.com>
2016-04-18 10:21:14 -0600

    Funcion borrar lista en calculadora

--- calculadora/js/calculadora.js
+++ calculadora/js/calculadora.js
@@ -24,3 +24,10 @@ function numeros(num){
 function operadores(ope){
 	operador = ope;
 }
+
+function borrar(){
+	operador = "";
+	document.calculadora.operando1.value = 0;
+	document.calculadora.operando2.value = 0;
+	document.calculadora.resultado.value = 0;
+}
